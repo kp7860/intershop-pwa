a8078b93f1fef3f8ce21ac8b533d205ba6d5f314
LucasHengelhaupt <l.hengelhaupt@intershop.com>
2023-06-08 15:08:36 +0200

    changed <a> into <button>, adapted code to work with the button click-event instead of an observable

--- src/app/shared/components/common/in-place-edit/in-place-edit.component.ts
+++ src/app/shared/components/common/in-place-edit/in-place-edit.component.ts
@@ -1,17 +1,4 @@
-import { DOCUMENT } from '@angular/common';
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  EventEmitter,
-  Inject,
-  OnDestroy,
-  Output,
-} from '@angular/core';
-import { Subject, fromEvent } from 'rxjs';
-import { filter, map, takeUntil } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'ish-in-place-edit',
@@ -19,70 +6,37 @@ import { filter, map, takeUntil } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./in-place-edit.component.scss'],
 })
-export class InPlaceEditComponent implements AfterViewInit, OnDestroy {
+export class InPlaceEditComponent {
   @Output() edited = new EventEmitter<void>();
   @Output() aborted = new EventEmitter<void>();
 
   private mode: 'view' | 'edit' = 'view';
-  private destroy$ = new Subject<void>();
 
-  hover: boolean;
+  constructor(private host: ElementRef) {}
 
-  constructor(
-    private host: ElementRef,
-    private cdRef: ChangeDetectorRef,
-    @Inject(DOCUMENT) private document: Document
-  ) {}
-
-  ngAfterViewInit() {
-    fromEvent(this.document, 'mousedown')
-      .pipe(
-        // only main button clicks
-        filter((event: MouseEvent) => !event.button),
-        map(({ target }) => this.host.nativeElement.contains(target)),
-        takeUntil(this.destroy$)
-      )
-      .subscribe(click => {
-        const newMode = click ? 'edit' : 'view';
-        if (newMode === 'view' && newMode !== this.mode) {
-          this.confirm();
-        }
-        this.mode = newMode;
-        this.cdRef.detectChanges();
-        if (this.mode === 'edit') {
-          setTimeout(() => {
-            this.host.nativeElement.querySelector('.form-control')?.focus();
-          }, 200);
-        }
-      });
+  changeEditMode() {
+    if (this.mode === 'edit') {
+      setTimeout(() => {
+        this.host.nativeElement.querySelector('.form-control')?.focus();
+      }, 200);
+    }
+    if (this.mode === 'view') {
+      this.confirm();
+      this.mode = 'edit';
+    }
   }
 
   confirm() {
     this.mode = 'view';
-    this.unsetHover();
     this.edited.emit();
   }
 
   cancel() {
     this.mode = 'view';
-    this.unsetHover();
     this.aborted.emit();
   }
 
   get viewMode() {
     return this.mode === 'view';
-  }
-
-  setHover() {
-    this.hover = true;
-  }
-
-  unsetHover() {
-    this.hover = false;
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
   }
 }
